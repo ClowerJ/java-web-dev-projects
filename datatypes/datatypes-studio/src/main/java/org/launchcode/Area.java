4a32d6d114b56e77938d3125be5c0d0d817b89f9
John Clowers <jclowers18@gmail.com>
2023-08-21 20:30:38 -0500

    Finish Bonus Mission

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -1,13 +1,27 @@
 package org.launchcode;
+import com.sun.source.tree.WhileLoopTree;
+
 import javax.net.ssl.SSLContext;
 import java.util.*;
 public class Area {
     public static void main(String[] args){
         Scanner input = new Scanner(System.in);
-
         System.out.println("Enter a radius:");
 
-        Double radius = input.nextDouble();
-        System.out.println("The area is: " + Math.PI * radius * radius);
+        if (input.hasNextDouble()){
+            Double radius = input.nextDouble();
+            while (radius < 0) {
+                System.out.println("Radius cannot be negative.");
+                System.out.println("Enter a radius:");
+                radius = input.nextDouble();
+            }
+            System.out.println("The area is: " + Circle.getArea(radius));
+
+        }
+         else {
+            System.out.println("Must be a number.");
+        }
+
+        input.close();
     }
 }
