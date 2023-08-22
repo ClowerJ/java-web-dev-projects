2ad0868d836acce2c6adc3a731324bf7b21cfc91
John Clowers <jclowers18@gmail.com>
2023-08-21 19:48:06 -0500

    Part A

--- datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
+++ datatypes/datatypes-studio/src/main/java/org/launchcode/Area.java
@@ -1,4 +1,13 @@
 package org.launchcode;
-
+import javax.net.ssl.SSLContext;
+import java.util.*;
 public class Area {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter a radius:");
+
+        Double radius = input.nextDouble();
+        System.out.println("The area is: " + Math.PI * radius * radius);
+    }
 }
